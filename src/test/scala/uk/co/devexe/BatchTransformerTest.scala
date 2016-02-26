ae45beaa95718a59e7e651f9d5adf9c25f0ba707
walpolrx <robkwalpole@gmail.com>
2016-02-26 16:46:40 +0000

    Now cleans up after itself

--- src/test/scala/uk/co/devexe/BatchTransformerTest.scala
+++ src/test/scala/uk/co/devexe/BatchTransformerTest.scala
@@ -1,24 +1,38 @@
 package uk.co.devexe
 
-import org.junit.Test
+import java.io.File
+
+import org.junit.{Assert, After, Test}
 
 import scala.collection.mutable
 
 /**
-  * Created by walpolrx on 24/02/2016.
+  * Created by Rob Walpole on 24/02/2016.
   */
 class BatchTransformerTest {
 
+  val sourceDir = "src/test/resources"
+  val targetDir = "src/test/resources"
+  val xsltPath = "src/test/resources/test.xsl"
+  val testOutputFile1 = new File(targetDir, "test1.html")
+  val testOutputFile2 = new File(targetDir, "test2.html")
+
   @Test
-  def testRun() = {
-    val sourceDir = "src/test/resources"
-    val targetDir = "src/test/resources"
-    val xsltPath = "src/test/resources/ptv.xsl"
+  def testRun() {
     val output = "html"
     val params = new mutable.HashMap[String, String]
-    params += ("target" -> "resources")
+    params += ("name" -> "Rob")
     val transformer = new BatchTransformer(sourceDir, targetDir, xsltPath, output, Some(params), None, None)
     transformer.run
+    Assert.assertTrue(testOutputFile1.exists)
+    Assert.assertTrue(testOutputFile2.exists)
+  }
+
+  @After
+  def tearDown() {
+    testOutputFile1.deleteOnExit
+    testOutputFile2.deleteOnExit
+
   }
 
 }
